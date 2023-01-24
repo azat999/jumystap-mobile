ba7c2620582cb97207d0de7b2bc7307c7bb80a2a
azat999 <31338185+azat999@users.noreply.github.com>
2023-01-24 16:42:13 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,3 +4,9 @@ const btn = document.querySelector('.btn');
 btn.onclick = function() {
     alert('Sorry! Azat Po has not yet learned the backend =(')
 }
+
+$('.mobile-menu').on('click',function(e) {
+    e.preventDefault();
+    $('.menu-btn').toggleClass('menu-active');
+    $('.topnav').toggleClass('menu-active');
+});
